dc9f7223a76d058b8016f3fb699c7e60e8207ad8
Lars Gyldenkærne <larg@widex.com>
2019-06-03 11:21:09 +0200

    MS ComServer Sample Restructured - Visual Studio 2019

--- core/extensions/COMServerDemo/COMClient/Program.cs
+++ core/extensions/COMServerDemo/COMClient/Program.cs
@@ -13,28 +13,4 @@ namespace COMClient
             Console.WriteLine($"\u03C0 = {pi}");
         }
     }
-
-    // The following classes are typically defined in a PIA, but for this example
-    // are being defined here to simplify the scenario.
-    namespace Activation
-    {
-        /// <summary>
-        /// Managed definition of CoClass 
-        /// </summary>
-        [ComImport]
-        [CoClass(typeof(ServerClass))]
-        [Guid(ContractGuids.ServerInterface)] // By TlbImp convention, set this to the GUID of the parent interface
-        internal interface Server : IServer
-        {
-        }
-
-        /// <summary>
-        /// Managed activation for CoClass
-        /// </summary>
-        [ComImport]
-        [Guid(ContractGuids.ServerClass)]
-        internal class ServerClass
-        {
-        }
-    }
 }
---
+++ core/extensions/COMServerDemo/COMServerPIA/Activation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// The following classes are typically defined in a PIA, but for this example
+// are being defined here to simplify the scenario.
+namespace Activation
+{
+    /// <summary>
+    /// Managed definition of CoClass 
+    /// </summary>
+    [ComImport]
+    [CoClass(typeof(ServerClass))]
+    [Guid(ContractGuids.ServerInterface)] // By TlbImp convention, set this to the GUID of the parent interface
+    public interface Server : IServer
+    {
+    }
+
+    /// <summary>
+    /// Managed activation for CoClass
+    /// </summary>
+    [ComImport]
+    [Guid(ContractGuids.ServerClass)]
+    public class ServerClass
+    {
+    }
+}
+
